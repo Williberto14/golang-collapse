3fcbdb49e6128f6b3e939794306f725781c90664
William Garcés <william.garces@kuasar.co>
2023-05-27 01:11:49 -0500

    feat: implement extencion code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "golang-collapse-methods" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('golang-collapse-methods.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from golang collapse methods!');
+	let disposable = vscode.commands.registerCommand('extension.collapseAllMethods', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const methodRanges = getMethodRanges(document);
+			if (methodRanges) {
+				collapseRanges(editor, methodRanges);
+			}
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getMethodRanges(document: vscode.TextDocument): vscode.Range[] | undefined {
+	const methodRanges: vscode.Range[] = [];
+	let isInMethod = false;
+	let openBracketsCount = 0;
+
+	for (let line = 0; line < document.lineCount; line++) {
+		const textLine = document.lineAt(line);
+		const lineText = textLine.text.trim();
+
+		if (lineText.startsWith('func ') || isInMethod) {
+			isInMethod = true;
+			methodRanges.push(textLine.range);
+			openBracketsCount += countCharOccurrences(lineText, '{');
+			openBracketsCount -= countCharOccurrences(lineText, '}');
+
+			if (openBracketsCount === 0) {
+				isInMethod = false;
+			}
+		}
+	}
+
+	return methodRanges.length > 0 ? methodRanges : undefined;
+}
+
+function countCharOccurrences(text: string, char: string): number {
+	return text.split(char).length - 1;
+}
+
+
+function collapseRanges(editor: vscode.TextEditor, ranges: vscode.Range[]) {
+	editor.selections = ranges.map(range => new vscode.Selection(range.start, range.start));
+	vscode.commands.executeCommand('editor.fold');
+}
+
+export function deactivate() { }
